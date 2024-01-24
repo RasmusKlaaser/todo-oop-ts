87ac97c86ae022306e7e4999642bf480423c548f
Rasmusklaaser@voco.ee <Rasmusklaaser@voco.ee>
2024-01-24 13:17:28 +0200

    Create new Todo from user data

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express'
-import todoRoutes from './routes/todos'
-import exp from 'constants'
+import { json } from 'body-parser'  
+
+import todoRoutes from '../routes/todos'
 
 const app = express()
+
+app.use(json())
 
 app.use('/todos', todoRoutes)
 
